bf0fbd3d484288fb6a2a37687d39b49961fffc8b
SergeyKalutsky <skalutsky@gmail.com>
2023-03-28 18:04:40 +0300

    added wiki

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from random import randint
 from api_token import TOKEN
 import discord
 import requests
+import wikipedia
+
+
+def search_wiki(search):
+    lst = wikipedia.search(search)
+    if lst:
+        res = wikipedia.page(lst[0])
+        return res.title, res.url
+
 
 def get_duck_url():
     res = requests.get('https://random-d.uk/api/random').json()
@@ -12,12 +21,19 @@ def get_fox():
     res = requests.get('https://randomfox.ca/floof/').json()
     return res['link']
 
+
 client = discord.Client(intents=discord.Intents.all())
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    if 'wiki' in message.content:
+        search = ' '.join(message.content.split(' ')[1:])
+        res = search_wiki(search)
+        if res:
+            await message.channel.send(f'{res[0]}]\n{res[1]}')
     if message.content == '!duck':
         url = get_duck_url()
         await message.channel.send(url)
