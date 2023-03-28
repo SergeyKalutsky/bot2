f099220b545d144b045ed430046a47fe245214a3
SergeyKalutsky <skalutsky@gmail.com>
2023-03-28 17:33:54 +0300

    добавил фукнции для лисы и утки фото

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from random import randint
 from api_token import TOKEN
 import discord
+import requests
+
+def get_duck_url():
+    res = requests.get('https://random-d.uk/api/random').json()
+    return res['url']
 
 
+def get_fox():
+    res = requests.get('https://randomfox.ca/floof/').json()
+    return res['link']
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -10,6 +18,12 @@ client = discord.Client(intents=discord.Intents.all())
 async def on_message(message):
     if message.author == client.user:
         return
+    if message.content == '!duck':
+        url = get_duck_url()
+        await message.channel.send(url)
+    if message.content == '!fox':
+        url = get_fox()
+        await message.channel.send(url)
     if message.content == 'как дела?':
         await message.channel.send('Хорошо как у тебя?')
     if message.content == 'с новым годом!':
